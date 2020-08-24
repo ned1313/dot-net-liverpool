5d3a8d606cba3064a1074d47e16da5a31d577e17
Ned Bellavance <ned@nedinthecloud.com>
2020-08-24 16:49:09 -0400

    adding backend

---
+++ 0-setup-remote-state/main.tf
@@ -0,0 +1,106 @@
+#############################################################################
+# VARIABLES
+#############################################################################
+
+variable "resource_group_name" {
+  type    = string
+  default = "terraform-state"
+}
+
+variable "location" {
+  type    = string
+  default = "eastus"
+}
+
+variable "naming_prefix" {
+  type    = string
+  default = "dotnet"
+}
+
+##################################################################################
+# PROVIDERS
+##################################################################################
+
+provider "azurerm" {
+  version = "~> 2.0"
+
+  features {}
+}
+
+##################################################################################
+# RESOURCES
+##################################################################################
+resource "random_integer" "sa_num" {
+  min = 10000
+  max = 99999
+}
+
+
+resource "azurerm_resource_group" "setup" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "sa" {
+  name                     = "${lower(var.naming_prefix)}${random_integer.sa_num.result}"
+  resource_group_name      = azurerm_resource_group.setup.name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+}
+
+resource "azurerm_storage_container" "ct" {
+  name                 = "terraform-state"
+  storage_account_name = azurerm_storage_account.sa.name
+
+}
+
+data "azurerm_storage_account_sas" "state" {
+  connection_string = azurerm_storage_account.sa.primary_connection_string
+  https_only        = true
+
+  resource_types {
+    service   = true
+    container = true
+    object    = true
+  }
+
+  services {
+    blob  = true
+    queue = false
+    table = false
+    file  = false
+  }
+
+  start  = timestamp()
+  expiry = timeadd(timestamp(), "17520h")
+
+  permissions {
+    read    = true
+    write   = true
+    delete  = true
+    list    = true
+    add     = true
+    create  = true
+    update  = false
+    process = false
+  }
+}
+
+
+##################################################################################
+# OUTPUT
+##################################################################################
+
+output "storage_account_name" {
+  value = azurerm_storage_account.sa.name
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.setup.name
+}
+
+output "sas_token" {
+    value = data.azurerm_storage_account_sas.state.sas
+}
