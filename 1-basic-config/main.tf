a1e74cbd85052801c0078b4d7d043d7ab98f5704
Oscar Villacis <oscar.villacis@outlook.com>
2021-08-16 13:11:40 -0400

    new branch

--- 1-basic-config/main.tf
+++ 1-basic-config/main.tf
@@ -59,12 +59,13 @@ module "network" {
   resource_group_name = azurerm_resource_group.vnet.name
   vnet_name = local.name
   address_space       = "10.0.0.0/16"
-  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24", "10.0.4.0/24"]
-  subnet_names        = ["subnet1", "subnet2", "subnet3", "subnet4"]
+  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24", "10.0.4.0/24", "10.0.5.0/24"]
+  subnet_names        = ["subnet1", "subnet2", "subnet3", "subnet4", "subnet5"]
 
   tags = {
     environment = "dev"
     costcenter  = "it"
+    project     = "TEST"
   }
 
   depends_on = [azurerm_resource_group.vnet]
